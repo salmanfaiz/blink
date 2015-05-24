70e763218dc554e352ebcd6187a822a034301461
salmanfaiz <salmanf86@yahoo.co.id>
2015-05-24 07:40:49 +0700

    Create kt blink4led_|s.faiz.s

---
+++ kt blink4led_|s.faiz.s
@@ -0,0 +1,36 @@
+int led0 = 13;
+int led1 = 12;
+int led2 = 11;
+int led3 = 10;
+void setup()
+{
+pinMode(13, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(10, OUTPUT);
+}
+void loop()
+{
+digitalWrite(led0, HIGH);
+delay(666);
+digitalWrite(led0. LOW);
+delay(666);
+{
+digitalWrite(led1, HIGH);
+delay(666);
+digitalWrite(led1, LOW);
+delay(666);
+}
+{
+digitalWrite(led2, HIGH);
+delay(666);
+digitalWrite(led2, LOW);
+delay(666)
+}
+{
+digitalWrite(led3, HIGH);
+delay(666);
+digitalWrite(led3, HIGH);
+delay(666);
+}
+}
